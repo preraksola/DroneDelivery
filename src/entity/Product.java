6c94be76694db5425c516e65f4d45962d7f4afa3
prerak <prerak6962@gmail.com>
2016-02-11 21:18:44 +0100

    Resolved conflicts

--- src/entity/Product.java
+++ src/entity/Product.java
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-package entity;
-
-public class Product {
-
-	private int productId;
-	private int numberOfProductItems;
-	private int weight;
-	public int getProductId() {
-		return productId;
-	}
-	public void setProductId(int productId) {
-		this.productId = productId;
-	}
-	public int getNumberOfProductItems() {
-		return numberOfProductItems;
-	}
-	public void setNumberOfProductItems(int numberOfProductItems) {
-		this.numberOfProductItems = numberOfProductItems;
-	}
-	public int getWeight() {
-		return weight;
-	}
-	public void setWeight(int weight) {
-		this.weight = weight;
-	}
-}
-=======
 package entity;
 
 public class Product {
