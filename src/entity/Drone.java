301e04304cd68dcce77bd7d43e5cb2bb5eaf86ef
srida <samyrida@hotmail.fr>
2016-02-11 21:00:42 +0100

    few changes (constructor, getter, setters)

--- src/entity/Drone.java
+++ src/entity/Drone.java
@@ -1,20 +1,25 @@
 package entity;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import sun.util.resources.CurrencyNames_ar_TN;
 
 public class Drone {
 
-	private int x;
-	private int y;
-	private Command currenCommand;
+	private Coordinates c;
+	private Command currentCommand;
 	private State currentState;
 	private int droneId;
 	private int maxCapacity;
 	private int waitNbTurn;
 	private HashMap<Product, Integer> productItems;
 	
+	public Drone(int droneId,int maxCapacity){
+		this.droneId=droneId;
+		this.maxCapacity=maxCapacity;
+		this.currentState=State.activated;
+	}
 	
 	
 	public HashMap<Product, Integer> getProductItems() {
@@ -47,25 +52,18 @@ public class Drone {
 	public void setDroneId(int droneId) {
 		this.droneId = droneId;
 	}
-	public int getX() {
-		return x;
+	public Coordinates getX() {
+		return c;
 	}
-	public void setX(int x) {
-		this.x = x;
-	}
-	
-	public int getY() {
-		return y;
-	}
-	public void setY(int y) {
-		this.y = y;
+	public void setCoordinates(Coordinates c) {
+		this.c = c;
 	}
 	
-	public Command getCurrenCommand() {
-		return currenCommand;
+	public Command getCurrentCommand() {
+		return currentCommand;
 	}
-	public void setCurrenCommand(Command currenCommand) {
-		this.currenCommand = currenCommand;
+	public void setCurrentCommand(Command currentCommand) {
+		this.currentCommand = currentCommand;
 	}
 	
 	public void wait(int nbTurn){
@@ -76,7 +74,7 @@ public class Drone {
 	
 	public void act(){
 		if(this.currentState!=State.wait){
-			switch(this.currenCommand){
+			switch(this.currentCommand){
 			case load:
 				break;
 			case unload:
@@ -94,12 +92,32 @@ public class Drone {
 	}
 	
 	public void move(int x,int y){
-		
+		this.c.setX(x);
+		this.c.setY(y);
+	}
+	
+	public Coordinates decision(ArrayList<Coordinates> ListOfCoordinates){
+		Coordinates bestDecision=new Coordinates();
+		for(int i=0;i<ListOfCoordinates.size();i++){
+			if(bestDecision==null){
+				bestDecision=ListOfCoordinates.get(i);
+			}else if(distance(this.c.getX(),this.c.getY(),bestDecision.getX(),bestDecision.getY())>distance(this.c.getX(),this.c.getY(),ListOfCoordinates.get(i).getX(),ListOfCoordinates.get(i).getY())){
+				bestDecision=ListOfCoordinates.get(i);
+			}
+		}
+		return bestDecision;
 	}
 	
 	public void load(int nbItem, Product prodType){
 		this.productItems.put(prodType, nbItem);
 		this.maxCapacity-=prodType.getWeight()*nbItem;
+	}
+	
+	public Double distance(int x1,int y1, int x2, int y2)
+	{
+		Double distance; 
+		distance = (Double)Math.ceil(Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2)));
+		return distance; 
 	}
 	
 	
